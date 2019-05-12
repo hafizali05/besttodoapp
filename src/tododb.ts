6e56407f38a6a5845c26bd228fbea71a3e9444d1
hafizali05 <hafizdidarali@hotmail.com>
2019-05-12 12:24:00 +0100

    setted up Todo db on mongo local

---
+++ src/tododb.ts
@@ -0,0 +1,24 @@
+import * as mongoose from "mongoose";
+
+const uri: string = "mongodb://127.0.0.1:27017/local";
+
+mongoose.connect(uri, (err: any) => {
+  if (err) {
+    console.log(err.message);
+  } else {
+    console.log("Succesfully Connected!");
+  }
+});
+
+export interface ITodo extends mongoose.Document {
+  title: string;
+  description: number;
+}
+
+export const TodoSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  description: { type: String, required: true }
+});
+
+const Todo = mongoose.model<ITodo>("Todo", TodoSchema);
+export default Todo;
